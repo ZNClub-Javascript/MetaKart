bf515cd57759773487e78dc103e920ce84626a37
end0cr1ne <f.warren210@gmail.com>
2015-10-21 23:23:16 +0530

    Added Captcha, signup page updated

--- src/controller/SignUpController.java
+++ src/controller/SignUpController.java
@@ -1,14 +1,25 @@
 package controller;
 
-import java.io.IOException;
-import java.sql.*;
-import java.util.*;
 import javax.mail.*;
-import javax.mail.internet.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Properties;
+import java.util.Random;
 
 /**
  * Created by fwarr on 27-Sep-15.
@@ -22,33 +33,73 @@ public class SignUpController extends HttpServlet {
         String from = "f.warren210@gmail.com";
 
         Properties props = new Properties();
-		props.put("mail.smtp.auth", "true");
-		props.put("mail.smtp.starttls.enable", "true");
-		props.put("mail.smtp.host", "smtp.gmail.com");
-		props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
 
-		Session session = Session.getInstance(props,
-		  new javax.mail.Authenticator() {
-			protected PasswordAuthentication getPasswordAuthentication() {
-				return new PasswordAuthentication("f.warren210@gmail.com", "motoxt720");
-			}
-		  });
+        Session session = Session.getInstance(props,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication("f.warren210@gmail.com", "motoxt720");
+                    }
+                });
 
 
-        try{
+        try {
             Random random = new Random();
             int vcode = random.nextInt();
             Class.forName("oracle.jdbc.driver.OracleDriver");
             Connection connection = DriverManager.getConnection("jdbc:oracle:thin:@//localhost:1521", "hr", "hr");
-            PreparedStatement statement = connection.prepareStatement("INSERT INTO USERS VALUES (?,?,?,0)");
-            statement.setString(1,request.getParameter("email"));
-            statement.setString(2,request.getParameter("password"));
-            statement.setInt(3,vcode);
+            PreparedStatement statement = connection.prepareStatement("INSERT INTO USERS VALUES (?,?,?,?,?)");
+            statement.setString(1, request.getParameter("email"));
+            statement.setString(2, request.getParameter("password"));
+
+
+            String urlParameters  = "secret=6Lc7RA8TAAAAAA5gr8g9_Bmov4zWmSuSIej4iSAQ&response=" + request.getParameter("g-recaptch-response");
+            byte[] postData       = urlParameters.getBytes( StandardCharsets.UTF_8 );
+            int    postDataLength = postData.length;
+            String requests       = "https://www.google.com/recaptcha/api/siteverify";
+            URL    url            = new URL( requests );
+            HttpURLConnection conn= (HttpURLConnection) url.openConnection();
+            conn.setDoOutput( true );
+            conn.setInstanceFollowRedirects( false );
+            conn.setRequestMethod( "POST" );
+            conn.setRequestProperty( "Content-Type", "application/x-www-form-urlencoded");
+            conn.setRequestProperty( "charset", "utf-8");
+            conn.setRequestProperty( "Content-Length", Integer.toString( postDataLength ));
+            conn.setUseCaches( false );
+            try( DataOutputStream wr = new DataOutputStream( conn.getOutputStream())) {
+                wr.write( postData );
+                wr.flush();
+                wr.close();
+            }
+            BufferedReader in = new BufferedReader(new InputStreamReader(
+                    conn.getInputStream()));
+            String inputLine;
+            StringBuffer responses = new StringBuffer();
+            while ((inputLine = in.readLine()) != null) {
+                responses.append(inputLine);
+            }
+            in.close();
+
+            // print result
+            System.out.println("POST Response" + responses.toString());
+
+
+            System.out.println("farmer: " + request.getParameter("user-type-farmer"));
+            //System.out.println("farmer: " + request.getParameter("g-recaptcha-response"));
+            //System.out.println("customer: " + request.getParameter("user-type-customer"));
+            statement.setInt(3, vcode);
+            statement.setInt(4,0);
+            if(request.getParameter("user-type-farmer").equals("on"))
+                statement.setInt(5,1);
+            else
+                statement.setInt(5,0);
             try {
                 statement.execute();
-            }
-            catch (Exception e){
-             response.sendRedirect("signup.jsp");
+            } catch (Exception e) {
+                response.sendRedirect("signup.jsp");
             }
             // Create a default MimeMessage object.
             MimeMessage message = new MimeMessage(session);
@@ -63,12 +114,12 @@ public class SignUpController extends HttpServlet {
             message.setSubject("Hey Emandi just mailed you!");
 
             // Now set the actual message
-            message.setText("And this is crazy, your account is ready.\n Mail us maybe?\nYour verification code link: http://localhost:8086/verify?vcode="+vcode);
+            message.setText("And this is crazy, your account is ready.\n Mail us maybe?\nYour verification code link: http://localhost:8086/verify?vcode=" + vcode);
 
             // Send message
             Transport.send(message);
             System.out.println("Sent message successfully....");
-        }catch (MessagingException mex) {
+        } catch (MessagingException mex) {
             mex.printStackTrace();
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
--- web/signup.jsp
+++ web/signup.jsp
@@ -13,62 +13,84 @@
     <script src="jquery.js"></script>
     <script src="semantic-ui/semantic.min.js"></script>
     <style>
-      body{
-        background: url(img/vegetables.jpg) fixed no-repeat;
-      }
-      .middle.center {
-        position: fixed;
-        top: 40%;
-        left: 40%;
-        width: 50%;
-      }
+        body {
+            background: url(img/vegetables.jpg) fixed no-repeat;
+        }
+
+        .middle.center {
+            position: fixed;
+            top: 40%;
+            left: 40%;
+            width: 50%;
+        }
     </style>
     <script>
-      $(function(){
-        $('.ui.form')
-                .form({
-                  fields: {
-                    username : 'email',
-                    password : ['minLength[6]', 'empty']
-                  }
-                });
-      });
+        $(function () {
+            $('.ui.form')
+                    .form({
+                        fields: {
+                            username: 'email',
+                            password: ['minLength[6]', 'empty']
+                        }
+                    });
+        });
+        $('.ui.radio.checkbox')
+                .checkbox()
+        ;
     </script>
+    <script src='https://www.google.com/recaptcha/api.js'></script>
 </head>
 <body>
 <div class="ui center aligned grid">
-  <div class="four wide column">
-    <h2 class="ui teal image header">
-      <img src="img/story_carrots.png" class="image">
-      <div class="content">
-        Sign up for the freshest veggies!
-      </div>
-    </h2>
-    <form class="ui large form" method="post" action="/signUpController">
-      <div class="ui stacked segment">
-        <div class="field">
-          <div class="ui left icon input">
-            <i class="user icon"></i>
-            <input type="email" name="email" placeholder="E-mail address">
-          </div>
+    <div class="four wide column">
+        <h2 class="ui teal image header">
+            <img src="img/story_carrots.png" class="image">
+
+            <div class="content">
+                Sign up for the freshest veggies!
+            </div>
+        </h2>
+        <form class="ui large form" method="post" action="/signUpController">
+            <div class="ui stacked segment">
+                <div class="field">
+                    <div class="ui left icon input">
+                        <i class="user icon"></i>
+                        <input type="email" name="email" placeholder="E-mail address">
+                    </div>
+                </div>
+                <div class="field">
+                    <div class="ui left icon input">
+                        <i class="lock icon"></i>
+                        <input type="password" name="password" placeholder="Password">
+                    </div>
+                </div>
+                <div class="inline fields">
+                    <label>User Type</label>
+                    <div class="field">
+                        <div class="ui radio checkbox">
+                            <input type="radio" name="user-type-farmer">
+                            <label>Farmer</label>
+                        </div>
+                    </div>
+                    <div class="field">
+                        <div class="ui radio checkbox">
+                            <input type="radio" name="user-type-farmer">
+                            <label>Customer</label>
+                        </div>
+                    </div>
+                </div>
+                <div class="g-recaptcha" data-sitekey="6Lc7RA8TAAAAAHDh4WnW8GHdnxrdEL7iaujXNaz7"></div>
+                <input type="submit" class="ui fluid large teal submit button" value="sign up!"/>
+            </div>
+
+            <div class="ui error message"></div>
+
+        </form>
+
+        <div class="ui message">
+            New to us? <a href="#">Sign Up</a>
         </div>
-        <div class="field">
-          <div class="ui left icon input">
-            <i class="lock icon"></i>
-            <input type="password" name="password" placeholder="Password">
-          </div>
-        </div>
-        <input type="submit" class="ui fluid large teal submit button" value="sign up!"/>
-      </div>
-
-      <div class="ui error message"></div>
-
-    </form>
-
-    <div class="ui message">
-      New to us? <a href="#">Sign Up</a>
     </div>
-  </div>
 </div>
 </body>
 </html>
